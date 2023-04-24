9fb63f05046fab7a032a2d795b6173bdefb37e3f
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-04-24 22:13:00 +0200

    started tv index

--- internal/repository/root.go
+++ internal/repository/root.go
@@ -142,7 +142,7 @@ func (r *MediaRepository) IndexMovie(movie *pkg.Movie, destination, fileDestinat
 	if err != nil {
 		return err
 	}
-	mediaData, err := pkg.RetrieveMediaData(fileDestination)
+	mediaData, err := pkg.RetrieveMediaData(path.Join(destination, fileDestination))
 	if err != nil {
 		return err
 	}
@@ -150,49 +150,17 @@ func (r *MediaRepository) IndexMovie(movie *pkg.Movie, destination, fileDestinat
 		MediaType:   MediaTypeMovie,
 		TmdbID:      movie.ID,
 		ReleaseDate: releaseDate,
-		Categories: func() []Category {
-			var categories []Category
-			for _, c := range movie.Categories {
-				alreadyInDB, err := r.getCategory(c.Name)
-				if err != nil {
-					categories = append(categories, Category{
-						Name: c.Name,
-					})
-				} else {
-					categories = append(categories, *alreadyInDB)
-				}
-			}
-			return categories
-		}(),
+		Categories:  *r.extractCategories(&movie.Categories),
 		Movies: []Movie{
 			{
 				Name: movie.Name,
 				MediaFile: MediaFile{
-					Filename: fileDestination,
-					Size:     mediaData.Size,
-					Duration: mediaData.Duration,
-					Codec:    VideoCodec(mediaData.Codec),
-					Audio: func() []Audio {
-						var audio = make([]Audio, 0)
-						for _, a := range mediaData.Audios {
-							audio = append(audio, Audio{
-								Codec:    AudioCodec(a.Codec),
-								Language: a.Language,
-								Bitrate:  a.Bitrate,
-							})
-						}
-						return audio
-					}(),
-					Subtitles: func() []Subtitle {
-						var subtitles = make([]Subtitle, 0)
-						for _, s := range mediaData.Subtitles {
-							subtitles = append(subtitles, Subtitle{
-								Codec:    SubtitleCodec(s.Codec),
-								Language: s.Language,
-							})
-						}
-						return subtitles
-					}(),
+					Filename:  fileDestination,
+					Size:      mediaData.Size,
+					Duration:  mediaData.Duration,
+					Codec:     VideoCodec(mediaData.Codec),
+					Audio:     *r.extractAudio(&mediaData.Audios),
+					Subtitles: *r.extractSubtitles(&mediaData.Subtitles),
 				},
 			},
 		},
@@ -206,6 +174,93 @@ func (r *MediaRepository) IndexMovie(movie *pkg.Movie, destination, fileDestinat
 		return db.Error
 	}
 	return nil
+}
+
+func (r *MediaRepository) IndexTvShow(tvShow *pkg.TVEpisode, destination, fileDestination string) error {
+	log.Printf("Indexing tv show %s", tvShow.Name)
+	releaseDate, err := time.Parse("2006-01-02", tvShow.TvReleaseDate)
+	if err != nil {
+		return err
+	}
+	epidodeReleaseDate, err := time.Parse("2006-01-02", tvShow.ReleaseDate)
+	if err != nil {
+		return err
+	}
+	mediaData, err := pkg.RetrieveMediaData(path.Join(destination, fileDestination))
+	if err != nil {
+		return err
+	}
+
+	media := Media{
+		MediaType:   MediaTypeEpisode,
+		TmdbID:      tvShow.ID,
+		ReleaseDate: epidodeReleaseDate,
+		Episodes: []Episode{
+			{
+				TvShow: TvShow{
+					// TODO check if tv show already exists
+					Media: Media{
+						MediaType:   MediaTypeTvShow,
+						TmdbID:      tvShow.TvShowID,
+						ReleaseDate: releaseDate,
+						Categories:  *r.extractCategories(&tvShow.Categories),
+					},
+					Name: tvShow.Name,
+				},
+				Name:      tvShow.Name,
+				NbEpisode: tvShow.Episode,
+				NbSeason:  tvShow.Season,
+				MediaFile: MediaFile{
+					Filename:  fileDestination,
+					Size:      mediaData.Size,
+					Duration:  mediaData.Duration,
+					Codec:     VideoCodec(mediaData.Codec),
+					Audio:     *r.extractAudio(&mediaData.Audios),
+					Subtitles: *r.extractSubtitles(&mediaData.Subtitles),
+				},
+			},
+		},
+	}
+	// TODO complete
+	return nil
+}
+
+func (r *MediaRepository) extractSubtitles(pkgSubtitles *[]pkg.SubtitleData) *[]Subtitle {
+	var subtitles = make([]Subtitle, len(*pkgSubtitles))
+	for i, s := range *pkgSubtitles {
+		subtitles[i] = Subtitle{
+			Codec:    SubtitleCodec(s.Codec),
+			Language: s.Language,
+		}
+	}
+	return &subtitles
+}
+
+func (r *MediaRepository) extractAudio(audiosData *[]pkg.AudioData) *[]Audio {
+	var audio = make([]Audio, len(*audiosData))
+	for i, a := range *audiosData {
+		audio[i] = Audio{
+			Codec:    AudioCodec(a.Codec),
+			Language: a.Language,
+			Bitrate:  a.Bitrate,
+		}
+	}
+	return &audio
+}
+
+func (r *MediaRepository) extractCategories(pkgCategories *[]pkg.Category) *[]Category {
+	var categories = make([]Category, len(*pkgCategories))
+	for i, c := range *pkgCategories {
+		alreadyInDB, err := r.getCategory(c.Name)
+		if err != nil {
+			categories[i] = Category{
+				Name: c.Name,
+			}
+		} else {
+			categories[i] = *alreadyInDB
+		}
+	}
+	return &categories
 }
 
 func (r *MediaRepository) clearDuplicatedMovie(tmdbID int, destination, fileDestination string) error {
