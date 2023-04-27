dacd6eebce2ab77cb8b775b1f763020be9c6bfce
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-04-27 19:03:04 +0200

    Remove test function for creating and retrieving MediaFile from the database

--- main.go
+++ main.go
@@ -4,9 +4,6 @@ import (
 	"flag"
 	"github.com/bingemate/media-indexer/cmd"
 	"github.com/bingemate/media-indexer/initializers"
-	"github.com/bingemate/media-indexer/internal/repository"
-	"gorm.io/gorm"
-	"gorm.io/gorm/clause"
 	"log"
 )
 
@@ -19,7 +16,6 @@ func main() {
 	}
 	logFile := initializers.InitLog(env.LogFile)
 	defer logFile.Close()
-
 	//test(db)
 	if *server {
 		log.Println("Starting server mode...")
@@ -28,29 +24,4 @@ func main() {
 		log.Println("Starting cli mode...")
 		cmd.ExecuteCli()
 	}
-}
-
-func test(db *gorm.DB) {
-	var mediaFile = repository.MediaFile{
-		Filename: "test.mkv",
-		Codec:    repository.VideoCodecH264,
-		Size:     1266565656.58,
-		Duration: 4600.58,
-		Subtitles: []repository.Subtitle{
-			{
-				Codec:    repository.SubtitleCodecSRT,
-				Language: "français",
-			},
-		},
-	}
-
-	/*	var subtitle = repository.Subtitles{
-		Codec:    repository.SRT,
-		Language: "français",
-	}*/
-
-	db.Create(&mediaFile)
-	var mediaFiles []repository.MediaFile
-	db.Joins(clause.Associations).Find(&mediaFiles)
-	log.Println(mediaFiles)
 }
