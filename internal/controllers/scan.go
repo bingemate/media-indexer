69df35f2237f2cd2e4a92ffc1a51449b76a2326a
Noé Larrieu-Lacoste <noelarrieulacoste@yahoo.fr>
2023-06-13 17:04:45 +0200

    feat: Add endpoints to get job logs and job name
    This commit adds two new endpoints to the scan controller that allow users to retrieve the logs of the last/current job and the name of the last/current job. The `/scan/logs` endpoint returns an array of `jobLogResponse`, while the `/scan/job-name` endpoint returns a string. Both endpoints have been added to `InitScanController`.

--- internal/controllers/scan.go
+++ internal/controllers/scan.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"github.com/bingemate/media-indexer/internal/features"
+	"github.com/bingemate/media-indexer/pkg"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,8 @@ type tvScanResponse struct {
 	Data []features.TVScannerResult `json:"data"`
 }
 
+type jobLogResponse pkg.JobLog
+
 func InitScanController(engine *gin.RouterGroup, movieScanner *features.MovieScanner, tvScanner *features.TVScanner) {
 	engine.POST("/movie", func(c *gin.Context) {
 		scanMovie(c, movieScanner)
@@ -20,44 +23,66 @@ func InitScanController(engine *gin.RouterGroup, movieScanner *features.MovieSca
 	engine.POST("/tv", func(c *gin.Context) {
 		scanTvShow(c, tvScanner)
 	})
+	engine.GET("/logs", func(c *gin.Context) {
+		getJobLogs(c)
+	})
+	engine.GET("/job-name", func(c *gin.Context) {
+		getJobName(c)
+	})
 }
 
 // @Summary		Scan Movies
 // @Description	Scan movies from the configured folder
 // @Tags			Scan
 // @Produce		json
-// @Success		200	{object} movieScanResponse
+// @Success		200	{string} string "Scan started"
 // @Failure		500	{object} errorResponse
 // @Router			/scan/movie [post]
 func scanMovie(c *gin.Context, movieScanner *features.MovieScanner) {
-	var result, err = movieScanner.ScanMovies()
+	var err = movieScanner.ScanMovies()
 	if err != nil {
 		c.JSON(500, errorResponse{
 			Error: err.Error(),
 		})
 		return
 	}
-	c.JSON(200, movieScanResponse{
-		Data: *result,
-	})
+	c.JSON(200, "Scan started")
 }
 
 // @Summary		Scan TV Shows
 // @Description	Scan TV Shows from the configured folder
 // @Tags			Scan
 // @Produce		json
-// @Success		200	{object} tvScanResponse
+// @Success		200	{string} string "Scan started"
 // @Failure		500	{object} errorResponse
 // @Router			/scan/tv [post]
 func scanTvShow(c *gin.Context, tvScanner *features.TVScanner) {
-	var result, err = tvScanner.ScanTV()
+	var err = tvScanner.ScanTV()
 	if err != nil {
 		c.JSON(500, errorResponse{
 			Error: err.Error(),
 		})
 		return
 	}
-	c.JSON(200, tvScanResponse{
-		Data: *result,
-	})
+	c.JSON(200, "Scan started")
+}
+
+// @Summary		Get Job Logs
+// @Description	Get the logs of the last / current job
+// @Tags			Scan
+// @Produce		json
+// @Success		200	{array} jobLogResponse
+// @Router			/scan/logs [get]
+func getJobLogs(c *gin.Context) {
+	c.JSON(200, pkg.PopJobLogs())
+}
+
+// @Summary		Get Job Name
+// @Description	Get the name of the last / current job
+// @Tags			Scan
+// @Produce		json
+// @Success		200	{string} string
+// @Router			/scan/job-name [get]
+func getJobName(c *gin.Context) {
+	c.JSON(200, pkg.GetJobName())
 }
